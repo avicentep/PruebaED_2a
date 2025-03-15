00750f983263c3d0b4de8d1a218a3d02a0a05d1f
Alberto Vicente <alvicentepasc@gmail.com>
2025-03-15 14:08:07 +0100

    Se ha documnetado la clase Nif.

--- src/main/java/com/casa/practicaed_2a/Nif.java
+++ src/main/java/com/casa/practicaed_2a/Nif.java
@@ -1,15 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.casa.practicaed_2a;
 
 /**
- *
- * @author ProfDiurno
+ * CLASE NIF.
+ * Clase que contiene los métodos necesarios para que los objetos NIF
+ * funcionen correctamente.
+ * @author Alberto Vicente
  */
 public class Nif {
-
+    // Atributos
     private int numero;
     private char letra;
 
@@ -18,31 +16,67 @@ public class Nif {
             = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D',
                 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L',
                 'C', 'K', 'E'};
-
-    private static char calcularLetra(int numero) {
-        return LETRAS[numero % 23];
-    }
-
+    
+    /**
+     * Constructor NIF por defecto.
+     * Constructor que inicializa los valores por defecto de los atributos
+     * de NIF.
+     */
     protected Nif() {
         this.numero = 0;
         this.letra = ' ';
     }
-
+    
+    /**
+     * Constructor NIF con int.
+     * Constructor que inicializa el valor del atributo numero haciendo uso del
+     * parámetro introducido y calcula la letra correspondiente haciendo uso del
+     * método calcularLetra(numero).
+     * @param numero: numero que se va a asignar al NIF.
+     */
     protected Nif(int numero) {
         this.numero = numero;
         this.letra = calcularLetra(numero);
     }
+    
+    /**
+     * calcularLetra.
+     * Método que calsula la letra en base al número pasado.
+     * @param numero: número del que se quiere calcular la letra.
+     * @return un char con la letra correspondiente de la operación.
+     */
+    private static char calcularLetra(int numero) {
+        return LETRAS[numero % 23];
+    }
 
+    /**
+     * toString.
+     * Método que devuelve el número con un guión y su letra correspondiente
+     * en formato String.
+     * @return lo dicho anteriormente.
+     */
     @Override
     public String toString() {
         return numero + "-" + letra;
     }
 
+    /**
+     * setNif.
+     * Método que permite sobreescribir los atributos de NIF.
+     * @param numero: número con el que se pretende sobreescribir el NIF existente.
+     */
     protected void setNif(int numero) {
         this.numero = numero;
         this.letra = calcularLetra(numero);
     }
 
+    /**
+     * equals.
+     * Método que comprueba que el objeto al que se está comparando es también
+     * de tipo Nif y devuelve true o false en base a si sus atributos se corresponden.
+     * @param obj: objeto a comparar.
+     * @return true si son iguales, false si no lo son.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
