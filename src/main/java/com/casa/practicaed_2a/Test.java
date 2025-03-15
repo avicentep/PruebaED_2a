1fc489b44fa9fcec2897c457e0cde6e80f210b07
Alberto Vicente <alvicentepasc@gmail.com>
2025-03-15 13:25:34 +0100

    Se ha creado el proyecto

---
+++ src/main/java/com/casa/practicaed_2a/Test.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.casa.practicaed_2a;
+
+/**
+ *
+ * @author alber
+ */
+public class Test {
+    public static void main(String[] args) {
+        Curso curso = new Curso("Prueba");
+        
+        System.out.println(curso);
+    }
+}
