a9c7d7c5a48fef909f8eccd113fd18c703a31b20
Alberto Vicente <alvicentepasc@gmail.com>
2025-03-15 13:58:53 +0100

    Se ha documnetado la clase Curso.

--- src/main/java/com/casa/practicaed_2a/Curso.java
+++ src/main/java/com/casa/practicaed_2a/Curso.java
@@ -1,29 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.casa.practicaed_2a;
 
 import java.util.TreeSet;
 
 /**
- *
- * @author ProfDiurno
+ * CLASE CURSO.
+ * Clase que contiene los métodos necesarios para que los objetos Clase
+ * funcionen correctamente.
+ * @author Alberto Vicente
  */
 public class Curso {
-
+    // Atributos
     private String nombre;
     private TreeSet<Persona> listaAlumnos;
 
-    protected String getNombre() {
-        return nombre;
-    }
-
+    /**
+     * Constructor Curso.
+     * Costructor de la clase curso que inicializa los valores de sus atributos.
+     * @param nombre: valor que se le quiere asignar al atributo nombre.
+     */
     public Curso(String nombre) {
         this.nombre = nombre;
         listaAlumnos = new TreeSet<>();
     }
-
+    
+    /**
+     * getNombre.
+     * Método que devuelve el valor del atributo nombre.
+     * @return el valor del atributo nombre.
+     */
+    protected String getNombre() {
+        return nombre;
+    }
+    
+    /**
+     * toString.
+     * Método que devuelve el conjunto de valores que componen al atributo 
+     * listaAlumnos en formato tabulado haciendo uso del método propio de la clase 
+     * Persona toString().
+     * @return un String que contiene los datos de todos los elementos del TreeSet
+     *         listaAlumnos.
+     */
     @Override
     public String toString() {
         String s = "--------------------" + nombre + "-----------------\n";
@@ -35,6 +51,11 @@ public class Curso {
         return s;
     }
 
+    /**
+     * aniadirAlumno.
+     * Método que permite añadir nuevos alumnos a listaAlumnos.
+     * @param p: alumno a introducir en la lista.
+     */
     public void aniadirAlumno(Persona p) {
         listaAlumnos.add(p);
     }
